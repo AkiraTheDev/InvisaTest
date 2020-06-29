822e8d23b6d6a267de340ede51dc290ab43cff85
AkiraTheDev <vanimusv@gmail.com>
2020-06-29 12:54:23 -0500

    midway through second test

--- test.py
+++ test.py
@@ -26,4 +26,28 @@ def degreesUnitTest():
         print("Succeded: Returned ", result)
 
 
-degreesUnitTest()
+#degreesUnitTest()
+
+import datetime
+
+def dateToDay(dateStr):
+    numbers = dateStr.split("/")
+    month = int(numbers[0])
+    day = int(numbers[1])
+    year = int(numbers[2])
+
+    givenDate = datetime.date(year, month, day)
+    startDate = datetime.date(1300,1,1)
+    #startDay = "Friday"
+
+    diffInDays = (givenDate - startDate).days
+    weekNum = diffInDays % 7
+    switcher = {
+        0:"Friday"
+        1:"Saturday"
+        2:"Sunday"
+        3:"Monday"
+        4:"Tuesday"
+    }
+
+dateToDay("1/1/1301")

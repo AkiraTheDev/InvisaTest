24a69ce2afae73b94c69090d1f65254836b8b028
AkiraTheDev <vanimusv@gmail.com>
2020-06-29 12:21:53 -0500

    Added angle function

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print("test")
+import string
+
+degreesPerHour = 360 / 12
+degreesPerMin = 360 / 60
+
+def getDegreesBetween(timeStr):
+    numbers = timeStr.split(":")
+    hour = int(numbers[0])
+    minutes = int(numbers[1])
+
+    hourDegrees = hour * degreesPerHour
+    minDegrees = minutes * degreesPerMin
+
+    angle = max(hourDegrees, minDegrees) - min(hourDegrees, minDegrees)
+    return angle
+
+
+
+#print(getDegreesBetween("1:30"))
